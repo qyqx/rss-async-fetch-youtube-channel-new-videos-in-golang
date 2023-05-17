dcb8cd0829fa37a77559690e40c86ef633b8580c
Beetsaber <guydibudre@desoz.com>
2023-05-17 14:35:30 +0000

    Upload New File

---
+++ RSS youtube feed/helpers/helpers.go
@@ -0,0 +1,67 @@
+package helpers
+
+
+import (
+    "bufio"
+	
+	"bytes"
+	"io"
+	
+	"runtime"
+	"os/exec"
+	"fmt"
+)
+
+
+// Readln returns a single line (without the ending \n) from the input buffered reader.
+// An error is returned iff there is an error with the buffered reader.
+func Readln(r *bufio.Reader) (string, error) {
+  var (isPrefix bool = true
+       err error = nil
+       line, ln []byte
+      )
+  for isPrefix && err == nil {
+      line, isPrefix, err = r.ReadLine()
+      ln = append(ln, line...)
+  }
+  return string(ln),err
+}
+
+func LineCounter(r io.Reader) (int, error) {
+    buf := make([]byte, 32*1024)
+    count := 0
+    lineSep := []byte{'\n'}
+
+    for {
+        c, err := r.Read(buf)
+        count += bytes.Count(buf[:c], lineSep)
+
+        switch {
+        case err == io.EOF:
+            return count, nil
+
+        case err != nil:
+            return count, err
+        }
+    }
+}
+
+func Openbrowser(url string) {
+	
+    var err error
+
+    switch runtime.GOOS {
+    case "linux":
+        err = exec.Command("xdg-open", url).Start()
+    case "windows":
+        err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+    case "darwin":
+        err = exec.Command("open", url).Start()
+    default:
+        err = fmt.Errorf("unsupported platform")
+    }
+    if err != nil {
+        panic(err)
+    }
+
+}
