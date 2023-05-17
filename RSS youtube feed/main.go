6c1b524864af21b3d62b5a2669b61dc2d745dd52
Beetsaber <guydibudre@desoz.com>
2023-05-17 20:08:40 +0000

    Bugfix xmlData.Body.Outlines

--- RSS youtube feed/main.go
+++ RSS youtube feed/main.go
@@ -592,12 +592,13 @@ func addToOpml(opmlFileName string, addXmlLinkToRss string, addNameToOpml string
 	var outlineWithNewUrl xmlFormats.Outline = xmlFormats.Outline{ Text:addNameToOpml, Title:addNameToOpml, Type:"rss", XMLURL:addXmlLinkToRss, HTMLURL:urlToSaveToOpml }
 	
 	for i := 0; i < len(xmlData.Body.Outlines); i++ {
-		for lineIndx, line:= range xmlData.Body.Outlines[len(xmlData.Body.Outlines)-1].Outlines {
+		for lineIndx, line:= range xmlData.Body.Outlines[i].Outlines {
 			if outlineWithNewUrl.XMLURL == line.XMLURL {
 				return errors.New("Feed already exists in body.outline "+strconv.Itoa(i+1)+" on line "+strconv.Itoa(lineIndx+1))
 			}
 		}
 	}
+	
 	// change xmlData.Body.Outlines by chaning last Outline in its Outlines array so that its Outlines has outlineWithNewUrl added to it
 	xmlData.Body.Outlines[len(xmlData.Body.Outlines)-1].Outlines = append(xmlData.Body.Outlines[len(xmlData.Body.Outlines)-1].Outlines, outlineWithNewUrl)
 	
